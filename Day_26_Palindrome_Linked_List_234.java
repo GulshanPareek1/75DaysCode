df03fb83be94c34a6c10138328316f3e3c16c01d
GulshanPareek1 <100085680+GulshanPareek1@users.noreply.github.com>
2023-11-04 20:35:08 +0530

    two approach for solution

--- Day_26_Palindrome_Linked_List_234.java
+++ Day_26_Palindrome_Linked_List_234.java
@@ -1,13 +1,8 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
+// there are two methods for this question 
+// one using stack and another using simple two pointer approach 
+// stack will take extra O(N) space and O(N) time complexity and 
+// normal two pointer will O(1) space and O(N) time complexity 
+
 class Solution {
     public boolean isPalindrome(ListNode head) {
         ListNode temp = head;
@@ -25,4 +20,41 @@ class Solution {
         }
         return true;
     }
+}
+
+
+
+
+
+public boolean isPalindrome(ListNode head) {
+    ListNode fast = head, slow = head;
+    while (fast != null && fast.next != null) {
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    if (fast != null) { // odd nodes: let right half smaller
+        slow = slow.next;
+    }
+    slow = reverse(slow);
+    fast = head;
+    
+    while (slow != null) {
+        if (fast.val != slow.val) {
+            return false;
+        }
+        fast = fast.next;
+        slow = slow.next;
+    }
+    return true;
+}
+
+public ListNode reverse(ListNode head) {
+    ListNode prev = null;
+    while (head != null) {
+        ListNode next = head.next;
+        head.next = prev;
+        prev = head;
+        head = next;
+    }
+    return prev;
 }
